17b10ac3761e973893feb4211cfe26d7af3ade06
Gokhan <gknsvsdata@gmail.com>
2022-07-03 18:21:15 +0300

    apiistek

--- src/components/Addtutorial.jsx
+++ src/components/Addtutorial.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+
 
 const Addtutorial = () => {
   return (
-    <div>Addtutorial</div>
+    <div>
+        <h1>Add Your Tutorial</h1>
+        <form>
+
+ 
+    <div className="title">
+      <label for="disabledTextInput" className="form-labe">Title</label> <br />
+      <input type="text" id="disabledTextInput" className="form-control" placeholder="Disabled input"/>
+    </div>
+    <div className="mb-3 desc">
+      <label for="disabledTextInput" className="form-label">Description</label> <br />
+      <input type="text" id="disabledTextInput" className="form-control" placeholder="Disabled input"/>
+    </div>
+    
+    
+    <button type="submit" className="btn btn-warning">Submit</button>
+ 
+</form>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
 import Addtutorial from "../components/Addtutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+    const [tutorials, setTutorials] = useState()
+
+    const url = "https://axios-example-cw.herokuapp.com/api/tutorials";
+
+    const getTutorials = async ()=>{
+        const {data} = await axios.get(url);
+        setTutorials(data);
+        // console.log(data);
+    }
+
+    useEffect(() => {
+      getTutorials();    
+    }, [])
+    console.log(tutorials);
+
+    // getTutorials()
   return (
     <div>
         <Addtutorial/>
